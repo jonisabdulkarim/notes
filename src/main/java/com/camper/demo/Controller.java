8eb694f7661769b9d72e319904ce5111041b6662
Jonis <jonis.abdulkarim@protonmail.com>
2019-11-25 20:27:05 +0000

    Create Index class

--- src/main/java/com/camper/demo/Controller.java
+++ src/main/java/com/camper/demo/Controller.java
@@ -1,13 +1,29 @@
 package com.camper.demo;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Controller {
 
-    @RequestMapping("/")
+    private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Welcome back, %s!%n";
+
+    @GetMapping("/")
     public String index() {
         return "Welcome to the index page!\n";
+    }
+
+    @PostMapping("/")
+    public Index index(@RequestParam(value="name") String name) {
+        long id = counter.incrementAndGet();
+        String content = String.format(template, name);
+        
+        return new Index(id, content);
     }
 }
---
+++ src/main/java/com/camper/demo/Index.java
@@ -0,0 +1,20 @@
+package com.camper.demo;
+
+public class Index {
+    
+    private final long id;
+    private final String content;
+
+    public Index(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
