c381fb9548afddd8a1ffe3102b3ea65a699829e9
Jonis <jonis.abdulkarim@protonmail.com>
2019-11-27 19:14:31 +0000

    Modify IndexController class to use standard MVC controller

--- src/main/java/com/camper/demo/IndexController.java
+++ src/main/java/com/camper/demo/IndexController.java
@@ -1,28 +1,14 @@
 package com.camper.demo;
 
-import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-@RestController
+@Controller
 public class IndexController {
 
-    private final AtomicLong counter = new AtomicLong();
-    private static final String template = "Welcome back, %s!%n";
-
-    @GetMapping("/")
+    @RequestMapping("/")
     public String index() {
         return "index";
     }
 
-    @PostMapping("/")
-    public Index index(@RequestParam(value="name") String name) {
-        long id = counter.incrementAndGet();
-        String content = String.format(template, name);
-        
-        return new Index(id, content);
-    }
 }
