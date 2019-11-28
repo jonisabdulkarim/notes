2bad35a777afca1f1676cc8dd3eab516d75a0f76
Jonis <jonis.abdulkarim@protonmail.com>
2019-11-28 20:29:29 +0000

    Code app.js, merge app and AccountData.js

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -9,10 +9,10 @@ class App extends React.Component {
     }
 
     componentDidMount() {
-        fetch("http://localhost:8080/api")
+        fetch("http://localhost:8080/api/accounts")
         .then(res => res.json())
         .then((data) => {
-            this.setState({ accounts: data })
+            this.setState({ accounts: data._embedded.accounts })
         })
         .catch(console.log)
     }
@@ -24,11 +24,30 @@ class App extends React.Component {
     }
 }
 
+class AccountData extends React.Component {
+    render() {
+        const accounts = this.props.accounts.map((account) => 
+            <Account key={account._links.self.href} account={account} />
+        );
+
+        return (
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Name</th>
+                    </tr>
+                    {accounts}
+                </tbody>
+            </table>
+        )
+    }
+}
+
 class Account extends React.Component {
     render() {
         return (
             <tr>
-                <td>this.props.account.name</td>
+                <td>{this.props.account.name}</td>
             </tr>
         )
     }
